49a96f59abb00664e3a6ea95b28ee43ac01ae327
LeRusseBesuhof <p.grad0ff@gmail.com>
2025-01-28 14:46:03 +0700

    Task List Interactor methods

--- EffectiveMobileTask/Components/Errors.swift
+++ EffectiveMobileTask/Components/Errors.swift
@@ -9,12 +9,16 @@ import Foundation
 
 enum Errors {
     case coreData
+    case network
+    case basic
 }
 
 extension Errors {
     var value: String {
         switch self {
         case .coreData: return "CoreData Error"
+        case .network: return "Network Error"
+        case .basic: return "Some Error"
         }
     }
 }
--- EffectiveMobileTask/Flows/TasksList/Interactor/TaskListInteractor.swift
+++ EffectiveMobileTask/Flows/TasksList/Interactor/TaskListInteractor.swift
@@ -16,7 +16,7 @@ protocol TaskListInteractorInput: AnyObject {
 
 protocol TaskListInteractorOutput: AnyObject {
     func sendError(withMessage: String, title: String)
-    func sendTasks(from taskList: [Task])
+    func send(_ tasksList: [Task])
 }
 
 final class TaskListInteractor {
@@ -30,6 +30,7 @@ final class TaskListInteractor {
         // MARK: - check if first program launch
         LaunchManager.isInitialLaunch()
     }
+    private var isLoading = false
     
     // MARK: - init
     init(networkManager: NetworkManagerOutput, storeManager: StoreManager) {
@@ -38,7 +39,7 @@ final class TaskListInteractor {
     }
     
     // MARK: - send request to create Task entity and save it
-    func saveLoadedTaskList(_ list: [RawTask], completion: @escaping (CoreDataError?) -> Void) {
+    func saveLoadedTaskList(_ list: [RawTask], completion: @escaping (Result<Void, CoreDataError>) -> Void) {
         // MARK: - using DispatcGroup to synchronize the result of asynchronous work
         let group = DispatchGroup()
         let queue = DispatchQueue(label: "saveTaskQueue")
@@ -69,7 +70,7 @@ final class TaskListInteractor {
                             savingError = error
                             if !isCompletionCalled {
                                 isCompletionCalled.toggle()
-                                completion(savingError)
+                                completion(.failure(error))
                             }
                         }
                     }
@@ -80,7 +81,116 @@ final class TaskListInteractor {
         // MARK: - notify main that everything's alright
         group.notify(queue: .main) {
             if savingError == nil, !isCompletionCalled {
-                completion(nil)
+                completion(.success(()))
+            }
+        }
+    }
+    
+    // MARK: - send request to fetch all tasks from storage
+    func loadTasksListFromStorage(completion: @escaping (Result<[Task], CoreDataError>) -> Void) {
+        storeManager.fetchTaskList { result in
+            completion(result)
+        }
+    }
+    
+    // MARK: - handles result and sends it or handles error
+    private func handleLoadResult(_ result: Result<[Task], CoreDataError>) {
+        switch result {
+        case .success(let tasksList):
+            output?.send(tasksList)
+        case .failure(let error):
+            handleError(error)
+        }
+    }
+    
+    // MARK: - manages errors and send them
+    private func handleError(_ error: Error) {
+        switch error {
+        case let error as CoreDataError:
+            output?.sendError(withMessage: error.localizedDescription, title: Errors.coreData.value)
+        case let error as NetworkError:
+            output?.sendError(withMessage: error.localizedDescription, title: Errors.network.value)
+        default:
+            output?.sendError(withMessage: error.localizedDescription, title: Errors.basic.value)
+        }
+    }
+    
+    // MARK: - send request to network manager to download raw tasks list
+    func downloadRawTasksList(completion: @escaping (Result<RawTaskList, NetworkError>) -> Void) {
+        networkManager.doRequest { result in
+            switch result {
+            case .success(let rawTasksList):
+                completion(.success(rawTasksList))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    // MARK: - download tasks from internet, save them and send
+    func downloadAndProcessTasks() {
+        downloadRawTasksList { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let rawTasksList):
+                saveLoadedTaskList(rawTasksList.todos) { [weak self] result in
+                    guard let self = self else { return }
+                    
+                    switch result {
+                    case .success:
+                        self.loadTasksListFromStorage { [weak self] result in
+                            guard let self = self else { return }
+                            
+                            self.handleLoadResult(result)
+                        }
+                    case .failure(let error):
+                        self.handleError(error)
+                    }
+                }
+            case .failure(let error):
+                self.handleError(error)
+            }
+        }
+    }
+    
+    // MARK: - check initial launch and load tasks from storage or from internet
+    func getTasksList() {
+        guard !isLoading else { return }
+        isLoading = true
+        
+        if isInitialLaunch {
+            downloadAndProcessTasks()
+        } else {
+            loadTasksListFromStorage { [weak self] result in
+                guard let self = self else { return }
+                
+                isLoading = false
+                handleLoadResult(result)
+            }
+        }
+    }
+    
+    // MARK: - send request to update task completion status
+    func updateTaskCompletionStatus(by id: Int, completion: @escaping (Result<Void, CoreDataError>) -> Void) {
+        storeManager.updateTask(with: .completionStatus, by: id) { result in
+            switch result {
+            case .success:
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    // MARK: - send request to remove task by ID
+    func removeTask(by id: Int, completion: @escaping (Result<Void, CoreDataError>) -> Void) {
+        storeManager.removeTask(by: id) { result in
+            switch result {
+            case .success:
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
     }
